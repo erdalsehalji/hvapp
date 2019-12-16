b818c3964798325fe707268c6c2fa736607fb41f
erdalsehalji <58055108+erdalsehalji@users.noreply.github.com>
2019-12-16 21:00:05 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <header>
+      <div id="navi">
+       
+        <ul>
+          <li>
+            <router-link class="a" to="/izbornik">O NAMA</router-link>
+          </li>
+          <li>
+            <router-link class="a" to="/pocetna">POČETNA</router-link>
+          </li>
+          <li>
+            <router-link class="a" to="/home">NUTRI CALC</router-link>
+          </li>
+          <li>
+            <router-link button type="button" class="button" to="/home">NAZAD</router-link>
+          <li>
+          <router-link button type="button" class="button" to="/login">LOGIN/SIGNUP</router-link>
+          </li>
+
+          <li style="float:right">
+            <router-link button type="button" id="if" class="button" to="/izbornik">Izbornik funkcija</router-link></li>
+        </ul>
+        <br>
+        <br>
+      </div>
+      </header>
+      <router-view />
+      <br>
+      <br>
+
+      <footer class="footer is-primaty">
+        <div class="container">
+          <div class="foot-top">
+            <div class="column">
+            <br>
+              <p>HEALTHVISOR APLIKACIJA ZA RAČUNANJE I PRAĆENJE 
+                TJELESNIH KARAKTERISTIKA
+              </p>
+            </div>
+          <div class="columns">
+            <router-link id="foti-li" to="/home">O NAMA</router-link>
+         <br>
+            <router-link class="a" to="/pocetna">NUTRI CALC</router-link>
+         <br>
+            <router-link id="foti-li" to="/home">POČETNA</router-link>
+          </div>
+          </div>
+        </div>
+      </footer>
+  </div>
+</template>
+
+<style>
+  .footer {
+    background-color: #333;
+    color: white;
+  }
+#if{
+  background-color: #4CAF50;
+}
+  #navi {
+    padding-bottom: 20 px;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+
+  li {
+    float: left;
+    border-right: 1px solid #bbb;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  li a:hover:not(.active) {
+    background-color: rgb(212, 255, 0);
+  }
+
+  .active {
+    background-color: #4CAF50;
+  }
+
+  #gridapp {
+    background-color: #333;
+    width: 100%;
+  }
+  .button {
+    background-color: orange;
+  }
+  #app{
+    background-color: rgb(84, 82, 82);
+  }
+
+</style>
---
+++ src/views/pocetna.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div id="home2" class="col-4"><br><br>
+        <h2>ZNATE LI OVU STATISTIKU?</h2><br>
+        <div class="card" style=""><img src="diduknow.png" class="card-img-top" alt=""></div><br>
+        <p>Pretilost je vodeći svijetski kroinični zdravstveni problem. <br></p>
+        <p>Pretilost u Europi se procjenuje na približno 30%. Po mišljeju SZO-a do 2030. čak 2.16 milijardi osoba imati
+          prekomjernu težinu,
+          dok će 1,
+          12 milijarda biti pretilo. </p><br>
+      </div>
+      <div id="home1" class="col-4"><br><br>
+        <h1>ZAŠTO BAŠ HEALTHVISOR?</h1><br>
+        <div class="card" style=""><img src="kako.jpg" class="card-img-top" alt=""></div><br>
+        <p>Pred vama je aplikacija koja će vam pokušati pomoći da živite zdraviji život.</p>
+        <p><b>Nudimo sljedeće opcije: </b></p>
+        <p>- kalkulator nutritivnih vrijednosti</p>
+        <p>- body kalkulator</p>
+        <p>- pračenje stanja tijela</p>
+        <p>- info o namirnicama</p>
+        <p><b>ŽELITE NAM SE OBRATITI?</b></p>
+        <div id="btnpoc">
+          <router-link id="fdb" button type="button" class="btn btn-primary btn-lg" to="/login">FEEDBACK</router-link>
+        </div>
+      </div>
+      <div id="home3" class="col-4"><br><br>
+        <h3>PROBAJTE HEALTHVISOR!</h3><br>
+        <div class="card" style=""><img src="logo.png" class="card-img-top" alt=""></div><br>
+        <div>
+          <p>Ukoliko samo želite isprobati healthvisor kliknite na gumb gost. </p>
+          <div id="btnpoc">
+            <router-link button type="button" class="btn btn-primary btn-lg" to="/izbornik">GOST</router-link>
+          </div><br>
+          <p>Želite li izraditi profil ili pristupiti postojećem kliknite login/signup gumb. </p>
+          <div id="btnpoc">
+            <router-link button type="button" class="btn btn-primary btn-lg" to="/login">LOGIN/SIGNUP</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+  #kako {
+    max-width: 50%;
+    max-height: 50%;
+    object-fit: contain;
+  }
+
+  p {
+    font-family: Chilanka;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 20px;
+    text-align: center;
+    color: whitesmoke;
+  }
+
+  h1 {
+    position: relative;
+    font-family: Chilanka;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 30px;
+    line-height: 40px;
+    text-align: center;
+    align-content: center;
+    color: orange;
+  }
+
+  h2 {
+    position: relative;
+    font-family: Chilanka;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 30px;
+    line-height: 40px;
+    text-align: center;
+    color: orange;
+  }
+
+  h3 {
+    position: relative;
+    font-family: Chilanka;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 30px;
+    line-height: 40px;
+    text-align: center;
+    color: orange;
+  }
+
+  #home1 {
+    position: relative;
+    background-color: rgb(68, 67, 67);
+  }
+
+  #home2 {
+    position: relative;
+    background-color: #333;
+  }
+
+  #home3 {
+    position: relative;
+    background-color: rgb(87, 85, 85);
+  }
+
+  #btnpoc {
+    text-align: center;
+  }
+</style>
